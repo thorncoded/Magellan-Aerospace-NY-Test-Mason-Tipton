536968b470170780caee53d30cf230f872cbcdcd
Mason Tipton <53275118+thorncoded@users.noreply.github.com>
2024-02-15 14:37:38 -0500

    Update ItemsController.cs - test connection

--- MagellanTest/Controllers/ItemsController.cs
+++ MagellanTest/Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 namespace MagellanTest.Controllers
 {
     [ApiController]
@@ -7,5 +8,37 @@ namespace MagellanTest.Controllers
     public class ItemsController : ControllerBase
     {
 
+        //would typically use ioptions pattern for this but this is fine
+        private readonly string connectionString;
+        public ItemsController(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+
+        //test connection, personal use, delete later.
+        [HttpGet("testconnection")]
+        public IActionResult TestDatabaseConnection()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Connection succeeded, return OK
+                    return Ok("Database connection successful.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //tell me that it failed
+                return StatusCode(500, $"Database connection failed: {ex.Message}");
+            }
+        }
+
+
+
     }
 }
+
