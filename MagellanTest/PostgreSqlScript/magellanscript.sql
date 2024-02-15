911138933b78fa10f89e1c3882ffa9fc17e737a1
Mason Tipton <53275118+thorncoded@users.noreply.github.com>
2024-02-15 12:59:18 -0500

    MAJOR CHANGE
    had misread earlier, this is database + table

--- MagellanTest/PostgreSqlScript/magellanscript.sql
+++ MagellanTest/PostgreSqlScript/magellanscript.sql
@@ -1,23 +1,35 @@
--- Create a database called Part, the date can be formatted later but type DATE is stored as YYYY-MM-DD.
-CREATE TABLE Part( 
+DROP DATABASE IF EXISTS Part;
+
+
+-- Create a database called Part, 
+CREATE DATABASE Part;
+
+
+\c part
+
+--DROP TABLE IF EXISTS public.item;
+
+-- the date can be formatted later but type DATE is stored as YYYY-MM-DD.
+
+CREATE TABLE public.item( 
     id SERIAL NOT NULL PRIMARY KEY,
     item_name VARCHAR(50) NOT NULL, 
-    parent_item INTEGER REFERENCES Part(id),
+    parent_item INTEGER REFERENCES item(id),
     cost INTEGER NOT NULL,
     req_date DATE NOT NULL 
 ); 
 
 -- Insert some data into the table
-INSERT INTO Part (item_name, cost, req_date) VALUES ('Item1', 500, '2024-02-20');
+INSERT INTO public.item (item_name, cost, req_date) VALUES ('Item1', 500, '2024-02-20');
 
-INSERT INTO Part (item_name, parent_item, cost, req_date) VALUES ('Sub1', 1, 200, '2024-02-10');
-INSERT INTO Part (item_name, parent_item, cost, req_date) VALUES ('Sub2', 1, 300, '2024-01-05');
+INSERT INTO public.item (item_name, parent_item, cost, req_date) VALUES ('Sub1', 1, 200, '2024-02-10');
+INSERT INTO public.item (item_name, parent_item, cost, req_date) VALUES ('Sub2', 1, 300, '2024-01-05');
 
-INSERT INTO Part (item_name, parent_item, cost, req_date) VALUES ('Sub3', 2, 300, '2024-01-02');
-INSERT INTO Part (item_name, parent_item, cost, req_date) VALUES ('Sub4', 2, 400, '2024-01-02');
+INSERT INTO public.item(item_name, parent_item, cost, req_date) VALUES ('Sub3', 2, 300, '2024-01-02');
+INSERT INTO public.item (item_name, parent_item, cost, req_date) VALUES ('Sub4', 2, 400, '2024-01-02');
 
-INSERT INTO Part (item_name, cost, req_date) VALUES ('Item2', 600, '2024-03-15');
-INSERT INTO Part (item_name, parent_item, cost, req_date) VALUES ('Sub1', 6, 200, '2024-02-25');
+INSERT INTO public.item(item_name, cost, req_date) VALUES ('Item2', 600, '2024-03-15');
+INSERT INTO public.item(item_name, parent_item, cost, req_date) VALUES ('Sub1', 6, 200, '2024-02-25');
 
 
 -- finding out
@@ -30,7 +42,7 @@ DECLARE
 BEGIN
     -- Check if the parent_item of the specified item_name_searching is not null
     IF EXISTS (
-        SELECT 1 FROM Part WHERE item_name = item_name_searching AND parent_item IS NOT NULL
+        SELECT 1 FROM public.item WHERE item_name = item_name_searching AND parent_item IS NOT NULL
     ) THEN
         RETURN NULL;
     END IF;
@@ -38,11 +50,11 @@ BEGIN
     -- fun w/recursion! >:)
     WITH RECURSIVE item_hierarchy AS (
         SELECT id, cost, parent_item
-        FROM Part
+        FROM public.item
         WHERE item_name = item_name_searching
         UNION ALL
         SELECT p.id, p.cost, p.parent_item
-        FROM Part p
+        FROM item p
         JOIN item_hierarchy ih ON p.parent_item = ih.id
     )
 
